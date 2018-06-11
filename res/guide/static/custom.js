f17ed606e24ad16c9d60131902b48eff05b793ea
ksozzy <3718757@qq.com>
2018-06-11 17:23:23 +0800

    611版本

---
+++ res/guide/static/custom.js
@@ -0,0 +1,20 @@
+window.CUSTOM_CONF = {
+  name: '装修报价助手',
+  vedioUrl: 'http://open.docer.wps.cn/#/home',
+  vedioTimeLength: '00:05:00',
+  vedioText: '视频演示',
+  tryBtn: {
+    text: '马上试用',
+    sub: '不需要，不再出现'
+  },
+  effects: {
+    centerMin: 'http://img1.file.cache.docer.com/storage/image/7/3/f/f/4/2a7c712232385bcdf83df7d0fb42f44f.gif',
+    table: 'http://img4.file.cache.docer.com/storage/image/b/2/d/0/b/f3ecbd8f2aafae87d868aff80945a0be.gif',
+    text1: 'http://img5.file.cache.docer.com/storage/image/0/2/3/5/e/9bdb00352c8b6eb337154c6f581080e4.gif',
+    text2: 'http://img8.file.cache.docer.com/storage/image/6/5/0/7/8/0788689854c17cf4061d615d7ceba388.gif',
+    waveBig: 'http://img4.file.cache.docer.com/storage/image/a/3/2/7/0/e558a7143cc4817921a515a571938400.gif',
+    waveSmall: 'http://img5.file.cache.docer.com/storage/image/5/b/5/9/0/10fb5400bac16aec5cb61a1f9383690f.gif',
+    waveRep: 'http://img6.file.cache.docer.com/storage/image/8/7/9/a/4/c9cb866379cfd43c96bc0a38a30c3a41.gif'
+  },
+  userAgreement: 'http://img3.file.cache.docer.com/storage/file/d/3/5/d/4/5abbd25e3076c8f9540ce036d1fb7f4f.html'
+}
