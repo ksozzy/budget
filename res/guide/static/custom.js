d839dd9afe36eb3edcfcd31cb8744b1085efd855
ksozzy <3718757@qq.com>
2018-06-21 14:40:19 +0800

    bug

--- res/guide/static/custom.js
+++ res/guide/static/custom.js
@@ -10,8 +10,8 @@ window.CUSTOM_CONF = {
   effects: {
     centerMin: 'http://img1.file.cache.docer.com/storage/image/7/3/f/f/4/2a7c712232385bcdf83df7d0fb42f44f.gif',
     table: 'http://img4.file.cache.docer.com/storage/image/b/2/d/0/b/f3ecbd8f2aafae87d868aff80945a0be.gif',
-    text1: 'http://img5.file.cache.docer.com/storage/image/0/2/3/5/e/9bdb00352c8b6eb337154c6f581080e4.gif',
-    text2: 'http://img8.file.cache.docer.com/storage/image/6/5/0/7/8/0788689854c17cf4061d615d7ceba388.gif',
+    text1: 'http://img5.file.cache.docer.com/storage/gif/c/0/f/a/9/05a3cffa0656deb2f0fac4d9a7fb4498.gif',
+    text2: 'http://img7.file.cache.docer.com/storage/gif/7/9/e/b/d/8e037e5394ab9226e183df75ba61b2df.gif',
     waveBig: 'http://img4.file.cache.docer.com/storage/image/a/3/2/7/0/e558a7143cc4817921a515a571938400.gif',
     waveSmall: 'http://img5.file.cache.docer.com/storage/image/5/b/5/9/0/10fb5400bac16aec5cb61a1f9383690f.gif',
     waveRep: 'http://img6.file.cache.docer.com/storage/image/8/7/9/a/4/c9cb866379cfd43c96bc0a38a30c3a41.gif'
