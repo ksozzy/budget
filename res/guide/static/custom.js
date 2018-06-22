5499590053bb95f965d2800d6d4cdc28b48a631d
ksozzy <3718757@qq.com>
2018-06-22 11:58:00 +0800

    bug

--- res/guide/static/custom.js
+++ res/guide/static/custom.js
@@ -9,7 +9,7 @@ window.CUSTOM_CONF = {
   },
   effects: {
     centerMin: 'http://img1.file.cache.docer.com/storage/image/7/3/f/f/4/2a7c712232385bcdf83df7d0fb42f44f.gif',
-    table: 'http://img4.file.cache.docer.com/storage/image/b/2/d/0/b/f3ecbd8f2aafae87d868aff80945a0be.gif',
+    table: 'http://img5.file.cache.docer.com/storage/gif/8/2/1/c/2/498e8570231eaec71537d533c7a8142d.gif,
     text1: 'http://img5.file.cache.docer.com/storage/gif/c/0/f/a/9/05a3cffa0656deb2f0fac4d9a7fb4498.gif',
     text2: 'http://img7.file.cache.docer.com/storage/gif/7/9/e/b/d/8e037e5394ab9226e183df75ba61b2df.gif',
     waveBig: 'http://img4.file.cache.docer.com/storage/image/a/3/2/7/0/e558a7143cc4817921a515a571938400.gif',
@@ -18,3 +18,4 @@ window.CUSTOM_CONF = {
   },
   userAgreement: 'http://img3.file.cache.docer.com/storage/file/d/3/5/d/4/5abbd25e3076c8f9540ce036d1fb7f4f.html'
 }
+http://img5.file.cache.docer.com/storage/gif/8/2/1/c/2/498e8570231eaec71537d533c7a8142d.gif
